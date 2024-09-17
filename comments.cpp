8be864d82defd8fcb206c27c422c4296b3e7dd8b
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2024-09-17 14:58:31 +0000

    comment based prog

---
+++ comments.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // dekho mujhe
+    /*not possible cause you are 
+    "comment"*/
+    cout << "Chiku's World"; // yo cow
+    return 0;
+}
