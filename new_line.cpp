8f8a9d28b2059e6a3e0f1ae501437a56efc417c8
nidhiv2017 <116021984+nidhiv2017@users.noreply.github.com>
2024-09-17 14:46:02 +0000

    some changes have been done

---
+++ new_line.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "meow meow \n meme cat";
+    cout << "crunchy cat";
+    cout << "\n";
+    cout << "depresso" <<"\n" ;
+    return 0;
+}
--- syntax.cpp
+++ syntax.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout <<"Helium is a new drug";
-
+    cout <<"Helium is a new drug ";
+ 
     return 0;
 }
